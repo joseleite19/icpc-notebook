24a336204b4b9a442fbb49e5ad3a24be540f6638
joseleite19 <leite.josemarcos@gmail.com>
2018-06-27 13:49:22 -0300

    add ordered set and fix tarjan bug

---
+++ code/ordered.cpp
@@ -0,0 +1,11 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#include <ext/pb_ds/detail/standard_policies.hpp>
+
+using namespace __gnu_pbds; // or pb_ds;
+
+template<typename T, typename B = null_type>
+using oset = tree<T, B, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// find_by_order / order_of_key
+
--- code/tarjan.cpp
+++ code/tarjan.cpp
@@ -6,8 +6,8 @@ void dfs(int u, int p = -1){
 			if(low[v] >= num[u]) u PONTO DE ARTICULACAO;
 			if(low[v] > num[u]){
 				ARESTA u->v PONTE;
-				low[u] = min(low[u], low[v]);
 			}
+			low[u] = min(low[u], low[v]);
 		}
 		else if(v != p) low[u] = min(low[u], num[v]);
 	}
