8d372c255b91f627c16e96d425848a11e0e75bea
José Marcos da Silva Leite <leite.josemarcos@gmail.com>
2018-07-15 22:53:00 -0300

    Update pollard_rho.cpp
    a bit safer version

--- code/pollard_rho.cpp
+++ code/pollard_rho.cpp
@@ -1,13 +1,16 @@
 ll rho(ll n){
 	if(n % 2 == 0) return 2;
 
-	ll d, c = llrand() % n, x = llrand() % n, y = x;
+	ll d, c, x, y;
 	do{
-		x = add(mul(x, x, n), c, n);
-		y = add(mul(y, y, n), c, n);
-		y = add(mul(y, y, n), c, n);
-		d = __gcd(abs(x - y), n);
-	}while(d == 1);
+		c = llrand() % n, x = llrand() % n, y = x;
+		do{
+			x = add(mul(x, x, n), c, n);
+			y = add(mul(y, y, n), c, n);
+			y = add(mul(y, y, n), c, n);
+			d = __gcd(abs(x - y), n);
+		}while(d == 1);
+	}while(d == n);
 	return d;
 }
 
