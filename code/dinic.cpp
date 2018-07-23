0dc1c1ef1de4d3ce8eeb2b744307e25eb88ba8a8
joseleite19 <leite.josemarcos@gmail.com>
2018-07-23 03:27:42 -0300

    using array as queue and now keeping flow on dinic

--- code/dinic.cpp
+++ code/dinic.cpp
@@ -1,52 +1,53 @@
-const int N = 205;
-const int E = 30006;
+const int N = 100005;
+const int E = 2000006;
 vector<int> g[N];
 
 int ne;
 struct Edge{
     int from, to;
-    ll cap;
+    ll flow, cap;
 } edge[E];
 
-int lvl[N], vis[N], pass, source, target, px[N];
+int lvl[N], vis[N], pass, source = N-2, target = N-1;
+int qu[N], qt, px[N];
 
 ll run(int s, ll minE){
     if(s == target) return minE;
 
     ll ans = 0;
 
-    for(; px[s] < g[s].size(); px[s]++){
+    for(; px[s] < (int)g[s].size(); px[s]++){
         int e = g[s][ px[s] ];
         auto &v = edge[e], &rev = edge[e^1];
-        if(lvl[v.to] != lvl[s]+1 || !v.cap) continue;
-        ll tmp = run(v.to, min(minE, v.cap));
-        v.cap -= tmp, rev.cap += tmp;
+        if(lvl[v.to] != lvl[s]+1 || v.flow >= v.cap) continue;
+        ll tmp = run(v.to, min(minE, v.cap-v.flow));
+        v.flow += tmp, rev.flow -= tmp;
         ans += tmp, minE -= tmp;
         if(minE == 0) break;
     }
     return ans;
 }
 
-int bfs(){
-
-    queue<int> q;
-    q.push(source);
+bool bfs(){
+    qt = 0;
+    qu[qt++] = source;
     lvl[source] = 1;
     vis[source] = ++pass;
 
-    while(!q.empty()){
-        int u = q.front(); q.pop();
+    for(int i = 0; i < qt; i++){
+        int u = qu[i];
         px[u] = 0;
+		if(u == target) return true;
 
         for(int e : g[u]){
             auto v = edge[e];
-            if(v.cap <= 0 || vis[v.to] == pass) continue;
+            if(v.flow >= v.cap || vis[v.to] == pass) continue;
             vis[v.to] = pass;
-            lvl[v.to] = lvl[u]+1;
-            q.push(v.to);
+            lvl[v.to] = lvl[u] + 1;
+            qu[qt++] = v.to;
         }
     }
-    return vis[target] == pass;
+    return false;
 }
 
 ll flow(){
@@ -55,8 +56,14 @@ ll flow(){
     return ans;
 }
 
-void addEdge(int u, int v, ll c){
-    edge[ne] = {u, v, c};
-    g[u].pb(ne++);
+void addEdge(int u, int v, ll c = 1, ll rc = 0){
+    edge[ne] = {u, v, 0, c};
+    g[u].push_back(ne++);
+    edge[ne] = {v, u, 0, rc};
+    g[v].push_back(ne++);
 }
 
+void reset_flow(){
+	for(int i = 0; i < ne; i++)
+		edge[i].flow = 0;
+}
