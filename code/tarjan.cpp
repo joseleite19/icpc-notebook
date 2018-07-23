32fbecf4daa9f352dff8e997bcf344dbc64d9e1e
joseleite19 <leite.josemarcos@gmail.com>
2018-07-23 04:30:09 -0300

    adding tarjan for SSC based on cp3

--- code/tarjan.cpp
+++ code/tarjan.cpp
@@ -11,3 +11,19 @@ void dfs(int u, int p = -1){
 	}
 }
 
+void tarjanSCC(int u){
+	low[u] = num[u] = cnt++;
+	vis[u] = 1;
+	S.push_back(u);
+	for(int v : g[u]){
+		if(!num[v]) tarjanSCC(v);
+		if(vis[v]) low[u] = min(low[u], low[v]);
+	}
+	if(low[u] == num[u]){
+		ssc[u] = ++ssc_cnt; int v;
+		do{
+			v = S.back(); S.pop_back(); vis[v] = 0;
+			ssc[v] = ssc_cnt;
+		}while(u != v);
+	}
+}
