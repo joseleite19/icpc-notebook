01546e441b286b758fb6eb2eb15e5cb9fd42837a
joseleite19 <leite.josemarcos@gmail.com>
2018-08-05 22:44:57 -0300

    adding wavelet tree

---
+++ code/ed/wavelet_tree.cpp
@@ -0,0 +1,68 @@
+template<typename T>
+class wavelet{
+    T L, R;
+    vector<int> l;
+	vector<T> sum; // <<
+	wavelet *lef, *rig;
+
+	int r(int i) const{ return i - l[i]; }
+
+public:
+	template<typename ITER>
+    wavelet(ITER bg, ITER en){
+		lef = rig = nullptr;
+        L = *bg, R = *bg;
+
+		for(auto it = bg; it != en; it++)
+            L = min(L, *it), R = max(R, *it);
+		if(L == R) return;
+        
+        T mid = L + (R - L)/2;
+		l.reserve(std::distance(bg, en) + 1);
+		sum.reserve(std::distance(bg, en) + 1);
+		l.push_back(0), sum.push_back(0);
+		for(auto it = bg; it != en; it++)
+			l.push_back(l.back() + (*it <= mid)),
+			sum.push_back(sum.back() + *it);
+
+		auto tmp = stable_partition(bg, en, [mid](T x){
+			return x <= mid;
+		});
+
+		if(bg != tmp) lef = new wavelet(bg, tmp);
+		if(tmp != en) rig = new wavelet(tmp, en);
+    }
+
+	~wavelet(){
+		delete lef;
+		delete rig;
+	}
+    
+	// 1 index, first is 1st
+    T kth(int i, int j, int k) const{
+        if(L >= R) return L;
+        int c = l[j] - l[i-1];
+        if(c >= k) return lef->kth(l[i-1]+1, l[j], k);
+        else return rig->kth(r(i-1)+1, r(j), k - c);
+    }
+	
+	// # elements > x on [i, j]
+	int cnt(int i, int j, T x) const{
+		if(L > x) return j - i + 1;
+		if(R <= x || L == R) return 0;
+		int ans = 0;
+		if(lef) ans += lef->cnt(l[i-1]+1, l[j], x);
+		if(rig) ans += rig->cnt(r(i-1)+1, r(j), x);
+		return ans;
+	}
+
+	// sum of elements <= k on [i, j]
+	T sumk(int i, int j, T k){
+		if(R <= k) return sum[j] - sum[i-1];
+		if(L == R || L > k) return 0;
+		int ans = 0;
+		if(lef) ans += lef->sumk(l[i-1]+1, l[j], k);
+		if(rig) ans += rig->sumk(r(i-1)+1, r(j), k);
+		return ans;
+	}
+};
