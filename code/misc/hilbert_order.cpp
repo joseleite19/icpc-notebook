fed74b4c78b5b82e31aea0982b512d620ebd8bbc
joseleite19 <leite.josemarcos@gmail.com>
2018-09-20 10:42:45 -0300

    including hilbert order

---
+++ code/misc/hilbert_order.cpp
@@ -0,0 +1,19 @@
+// maybe use B = n / sqrt(q)
+inline int64_t hilbertOrder(int x, int y, int pow = 21, int rotate = 0) {
+	if(pow == 0) return 0;
+	int hpow = 1 << (pow-1);
+	int seg = (x < hpow) ? (
+		(y < hpow) ? 0 : 3
+	) : (
+		(y < hpow) ? 1 : 2
+	);
+	seg = (seg + rotate) & 3;
+	const int rotateDelta[4] = {3, 0, 0, 1};
+	int nx = x & (x ^ hpow), ny = y & (y ^ hpow);
+	int nrot = (rotate + rotateDelta[seg]) & 3;
+	int64_t subSquareSize = int64_t(1) << (2*pow - 2);
+	int64_t ans = seg * subSquareSize;
+	int64_t add = hilbertOrder(nx, ny, pow-1, nrot);
+	ans += (seg == 1 || seg == 2) ? add : (subSquareSize - add - 1);
+	return ans;
+}
