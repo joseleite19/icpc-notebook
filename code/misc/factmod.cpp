50043f3974d37ee7ea55ab077b1c2a53417967e0
joseleite19 <leite.josemarcos@gmail.com>
2018-10-09 16:55:19 -0300

    including modular factorial and all how to enumarate all submasks

---
+++ code/misc/factmod.cpp
@@ -0,0 +1,27 @@
+// Compute (1*2*...*(p-1)*1*(p+1)*(p+2)*..*n) % p
+// in O(p*lg(n))
+
+int factmod(int n, int p){
+    int ans = 1;
+    while(n > 1){
+        for(int i = 2; i <= n % p; i++)
+            ans = (ans * i) % p;
+        n /= p;
+        if(n % 2) ans = p - ans;
+    }
+    return ans % p;
+}
+
+int fac_pow(int n, int p){
+    int ans = 0;
+    while(n) n /= p, ans += n;
+    return ans;
+}
+
+int C(int n, int k, int p){
+    if(fac_pow(n, p) > fac_pow(n-k, p) + fac_pow(k, p))
+        return 0;
+    int tmp = factmod(k, p) * factmod(n-k, p);
+    return (f_exp(tmp, p-2, p) * factmod(n, p)) % p;
+}
+
---
+++ code/misc/submasks.cpp
@@ -0,0 +1,6 @@
+// loop through all submask of a given bitmask
+// it does not include mask 0
+for(int sub = mask; sub; sub = (sub-1)&mask){
+
+}
+
