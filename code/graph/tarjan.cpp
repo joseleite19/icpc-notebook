d426a23d18f7d5fb5b858fb0202e2264cb8f3c1b
José Marcos da Silva Leite <leite.josemarcos@gmail.com>
2019-02-18 19:45:48 -0300

    Update tarjan.cpp

--- code/graph/tarjan.cpp
+++ code/graph/tarjan.cpp
@@ -4,7 +4,7 @@ void dfs(int u, int p = -1){
 	for(int v : g[u]){
 		if(!num[v]){
 			dfs(v, u);
-            if(v == root) cnt++;
+           		if(u == root) cnt++;
 			if(low[v] >= num[u]) u PONTO DE ARTICULACAO;
 			if(low[v] > num[u]) ARESTA u->v PONTE;
 			low[u] = min(low[u], low[v]);
