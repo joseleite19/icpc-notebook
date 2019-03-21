011f86d88e143cb93bf4173b1b0b73ce37e619c0
luisfbgs <arueluis@gmail.com>
2019-03-21 17:23:27 -0300

    suffix array coisado

--- code/string/suffix_array.cpp
+++ code/string/suffix_array.cpp
@@ -1,54 +1,71 @@
+#include <bits/stdc++.h>
 
-char s[N];
-int n, sa[N], tsa[N], lcp[N], r[N], nr[N], c[N];
+using namespace std;
 
-void sort(int k, int mx){
-    mx += 2;
-    memset(c, 0, sizeof(int) * mx);
-    for(int i = 0; i < n; i++) c[i + k < n ? r[i+k]+2 : 1]++;
-    partial_sum(c, c+mx, c);
-    int t;
+const int N = 2e6;
+
+int lcp[N], c[N];
+
+// Caractere final da string '\0' esta sendo considerado parte da string s
+void build_sa(char s[], int n, int a[]){
+    const int A = 300; // Tamanho do alfabeto
+    int c1[n], a1[n], h[n + A];
+    memset(h, 0, sizeof h);
+
+    for(int i = 0; i < n; i++) {
+        c[i] = s[i];
+        h[c[i] + 1]++;
+    }
+
+    partial_sum(h, h + A, h);
     for(int i = 0; i < n; i++)
-		t = sa[i]+k < n ? r[ sa[i]+k ]+1 : 0,
-        tsa[ c[t]++ ] = sa[i];
-    memcpy(sa, tsa, sizeof(int) * n);
-}
+        a[h[c[i]]++] = i;
+    for(int i = 0; i < n; i++)
+        h[c[i]]--;
 
-void build_sa(){
-
-    for(int i = 0; i < n; i++) sa[i] = i, r[i] = s[i];
-
-    int t = 300, a, b;
-    for(int sz = 1; sz < n; sz *= 2){
-        sort(sz, t), sort(0, t);
-        t = nr[ sa[0] ] = 0;
-        for(int i = 1; i < n; i++){
-            a = sa[i]+sz < n ? r[ sa[i]+sz ] : -1;
-            b = sa[i-1]+sz < n ? r[ sa[i-1]+sz ] : -1;
-            nr[ sa[i] ] = r[ sa[i] ] == r[ sa[i-1] ] && a == b ? t : ++t;
+    for(int L = 1; L < n; L <<= 1) {
+        for(int i = 0; i < n; i++) {
+            int j = (a[i] - L + n) % n;
+            a1[h[c[j]]++] = j;
         }
-        if(t == n-1) break;
-        memcpy(r, nr, sizeof(int) * n);
+        
+        int cc = -1;
+        for(int i = 0; i < n; i++) {
+            if(i == 0 || c[a1[i]] != c[a1[i-1]] || c[(a1[i] + L) % n] != c[(a1[i-1] + L) % n])
+                h[++cc] = i;
+            c1[a1[i]] = cc;
+        }
+
+        memcpy(a, sa1, sizeof sa1);
+        memcpy(c, c1, sizeof c1);
     }
 }
 
-void build_lcp(){ // lcp[i] = lcp(s[:i], s[:i+1])
+void build_lcp(char s[], int n, int a[]){ // lcp[i] = lcp(s[:i], s[:i+1])
     int k = 0;
-    for(int i = 0; i < n; i++) r[ sa[i] ] = i;
 
     for(int i = 0; i < n; i++){
-        if(r[i] == n-1) k = 0;
+        if(c[i] == n-1) k = 0;
         else{
-            int j = sa[r[i]+1];
+            int j = a[c[i]+1];
             while(i+k < n && j+k < n && s[i+k] == s[j+k]) k++;
         }
-        lcp[r[i]] = k;
+        lcp[c[i]] = k;
         if(k) k--;
     }
 }
 
 int comp_lcp(int i, int j){
     if(i == j) return n - i;
-    if(r[i] > r[j]) swap(i, j);
-    return min(lcp[k] for k in [r[i], r[j]-1]);
+    if(c[i] > c[j]) swap(i, j);
+    return min(lcp[k] for k in [c[i], c[j]-1]);
+}
+
+int main() {
+    scanf("%s", s);
+    build_sa();
+    for(int i = 0; i < n; i++){
+        printf("%d ", sa[i]);
+    }
+    printf("\n");
 }
