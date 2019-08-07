60b32169dac5efa1134dd1e3507cc152a6e1461b
José Marcos da Silva Leite <leite.josemarcos@gmail.com>
2019-08-07 10:46:37 -0300

    Add basis size info

--- code/gauss_xor.cpp
+++ code/gauss_xor.cpp
@@ -1,17 +1,18 @@
 const ll MAX = 1e9;
 const int LOG_MAX = 64 - __builtin_clzll((ll)MAX);
 
-struct gauss {
+struct Gauss {
     array<ll, LOG_MAX> vet;
-    gauss(){}
-    gauss(ll val) {
-        add(val);
+    int size;
+    Gauss() size(0) {}
+    Gauss(vector<ll> vals) size(0) {
+        for(ll val : vals) add(val);
     }
     bool add(ll val) {
-        int sig = LOG_MAX;
         for(int i = 0; i < LOG_MAX; i++) if(val & (1LL << i)) {
-            if(!vet[i]) {
+            if(vet[i] == 0) {
                 vet[i] = val;
+                size++;
                 return true;
             }
             val ^= vet[i];
