5187e84620e3cb05717ffe061f2ab7e1dd048fca
José Marcos da Silva Leite <leite.josemarcos@gmail.com>
2019-08-07 10:41:49 -0300

    improve gauss_xor readability and constant factor

--- code/gauss_xor.cpp
+++ code/gauss_xor.cpp
@@ -1,25 +1,21 @@
 const ll MAX = 1e9;
-const ll LOG_MAX = 64 - __builtin_clzll((ll)MAX);
+const int LOG_MAX = 64 - __builtin_clzll((ll)MAX);
 
-struct gauss{
-    vector<ll> vet;
+struct gauss {
+    array<ll, LOG_MAX> vet;
     gauss(){}
-    gauss(ll val){
-        if(val) vet.push_back(val);
+    gauss(ll val) {
+        add(val);
     }
-    void add(ll val){
+    bool add(ll val) {
         int sig = LOG_MAX;
-        for(int i = 0; i < (int)vet.size(); i++){
-            while(!(vet[i] & (1LL << sig))) sig--;
-            if(val & (1LL << sig)) val ^= vet[i];
+        for(int i = 0; i < LOG_MAX; i++) if(val & (1LL << i)) {
+            if(!vet[i]) {
+                vet[i] = val;
+                return true;
+            }
+            val ^= vet[i];
         }
-        if(!val) return;
-        sig = LOG_MAX;
-        while(!(val & (1 << sig))) sig--;
-        for(auto &x : vet) if(x & (1LL << sig)) x ^= val;
-        vet.push_back(val);
-        for(int i = (int)vet.size() - 2; i >= 0 && vet[i] < vet[i + 1]; i--){
-            swap(vet[i], vet[i + 1]);
-        }
+        return false;
     }
 };
