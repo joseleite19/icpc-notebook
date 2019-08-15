f3f5e44a6a6f09fc5320269abf5d50bd10d93499
joseleite19 <leite.josemarcos@gmail.com>
2019-08-15 02:08:49 -0300

    improve aho constant and remove old code

--- code/string/aho_corasick.cpp
+++ code/string/aho_corasick.cpp
@@ -1,30 +1,37 @@
-map<char, int> to[N];
-int ne = 2, term[N], fail[N];
-void add_string(char *str){
+int to[N][A];
+int ne = 2, fail[N], term[N];
+void add_string(const char *str, int id){
     int p = 1;
     for(int i = 0; str[i]; i++){
-        if(!to[p][ str[i] ]) to[p][ str[i] ] = ne++;
-        p = to[p][ str[i] ];
+        int ch = str[i] - 'a';
+        if(!to[p][ch]) to[p][ch] = ne++;
+        p = to[p][ch];
     }
-    term[p] = 1;
-}
-int go(int s, char c){
-    while(s && !to[s].count(c)) s = fail[s];
-    if(to[s].count(c)) return to[s][c];
-    return s;
+    term[p]++;
 }
 void init(){
+    for(int i = 0; i < ne; i++) fail[i] = 1;
     queue<int> q; q.push(1);
     int u, v; char c;
     while(!q.empty()){
         u = q.front(); q.pop();
-        for(auto w : to[u]){
-            tie(c, v) = w; q.push(v);
-            if(u != 1){
-                fail[v] = go(fail[u], c);
-                term[v] |= term[ fail[v] ];
+        for(int i = 0; i < A; i++){
+            if(to[u][i]){
+                v = to[u][i]; q.push(v);
+                if(u != 1){
+                    fail[v] = to[ fail[u] ][i];
+                    term[v] += term[ fail[v] ];
+                }
             }
+            else if(u != 1) to[u][i] = to[ fail[u] ][i];
+            else to[u][i] = 1;
         }
     }
 }
-
+void clean() {
+    memset(to, 0, ne * sizeof(to[0]));
+    memset(fail, 0, ne * sizeof(fail[0]));
+    memset(term, 0, ne * sizeof(term[0]));
+    memset(to, 0, ne * sizeof(to[0]));
+    ne = 2;
+}
