aa69361fc1529338f28a8811acbafb7d248a49ff
José Marcos da Silva Leite <leite.josemarcos@gmail.com>
2019-09-09 01:27:48 -0300

    update simpson's base case to cover edge cases

--- code/math/simpson.cpp
+++ code/math/simpson.cpp
@@ -1,19 +1,16 @@
-inline double simpson(double fl,double fr,double fmid,double l,double r) { return (fl+fr+4.0*fmid)*(r-l)/6.0; }
-double rsimpson(double slr,double fl,double fr,double fmid,double l,double r)
-{
+inline double simpson(double fl,double fr,double fmid,double l,double r) {
+	return (fl + fr + 4.0 * fmid) * (r - l) / 6.0;
+}
+double rsimpson(double slr,double fl,double fr,double fmid,double l,double r) {
 	double mid = (l+r)*0.5;
-	double fml = f((l+mid)*0.5);
-	double fmr = f((mid+r)*0.5);
-	double slm = simpson(fl,fmid,fml,l,mid);
-	double smr = simpson(fmid,fr,fmr,mid,r);
-	if(fabs(slr-slm-smr) < eps) return slm+smr;
-	return rsimpson(slm,fl,fmid,fml,l,mid)+rsimpson(smr,fmid,fr,fmr,mid,r);
+	double fml = f((l+mid)*0.5), fmr = f((mid+r)*0.5);
+	double slm = simpson(fl, fmid, fml, l, mid);
+	double smr = simpson(fmid, fr, fmr, mid, r);
+	if(fabs(slr-slm-smr) < eps and r - l < delta) return slr;
+	return rsimpson(slm,fl,fmid,fml,l,mid) + rsimpson(smr,fmid,fr,fmr,mid,r);
 }
 double integrate(double l,double r) {
 	double mid = (l+r)*0.5;
-	double fl = f(l);
-	double fr = f(r);
-	double fmid = f(mid);
+	double fl = f(l), fr = f(r), fmid = f(mid);
 	return rsimpson(simpson(fl,fr,fmid,l,r),fl,fr,fmid,l,r);
 }
-
