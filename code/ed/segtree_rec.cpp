a5ce418cbb9a7a3650653dfb51e290fb759d25ea
gtaumaturgo <gtaumaturg@gmail.com>
2019-09-17 15:20:50 -0300

    added build method to recursive segtree and made some minor changes like inline methods

--- code/ed/segtree_rec.cpp
+++ code/ed/segtree_rec.cpp
@@ -5,16 +5,29 @@ class  SegTree{
 
     int el_neutro = -oo;
 
-    int f(int a, int b){
+    inline int f(int a, int b){
         return max(a,b);
     }
+    inline int left(int i) {return 2 * i + 1;};
+    inline int right(int i) {return 2 * i + 2;};
+    void build(int sti, int stl, int str, vi& nums) {
+        if(stl == str) {
+            st[sti] = nums[stl];
+            return;
+        }
+        int mid = (stl + str) / 2;
+        build(left(sti), stl, mid, nums);
+        build(right(sti), mid + 1, str, nums);
+        st[sti] = f(st[left(sti)], st[right(sti)]);
+    }
+
     void propagate(int sti, int stl, int str){
         if(lazy[sti]){
             st[sti] += lazy[sti];
             if(stl != str)
             {
-                lazy[sti*2 + 1] += lazy[sti];
-                lazy[sti*2 + 2] += lazy[sti];
+                lazy[left(sti)] += lazy[sti];
+                lazy[right(sti)] += lazy[sti];
             }
             lazy[sti] = 0;
         }
@@ -31,7 +44,7 @@ class  SegTree{
 
         int mid = (str+stl)/2;
 
-        return f(query(sti*2+1,stl,mid,l,r),query(sti*2+2,mid+1,str,l,r));
+        return f(query(left(sti),stl,mid,l,r),query(right(sti),mid+1,str,l,r));
     }
     void update_range(int sti, int stl, int str, int l,int r, int amm){
         propagate(sti, stl, str);
@@ -43,9 +56,9 @@ class  SegTree{
         if(stl > r  or str < l)
             return;
         int mid = (stl + str)/2;
-        update_range(sti*2+1,stl,mid,l,r,amm);
-        update_range(sti*2+2,mid+1,str,l,r,amm);
-        st[sti] = f(st[sti*2+1],st[sti*2+2]);
+        update_range(left(sti),stl,mid,l,r,amm);
+        update_range(right(sti),mid+1,str,l,r,amm);
+        st[sti] = f(st[left(sti)],st[right(sti)]);
     }
     void update(int sti, int stl, int str, int i, int amm){
         propagate(sti, stl, str);
@@ -56,12 +69,13 @@ class  SegTree{
         if(stl > i or str < i)
             return;
         int mid = (stl + str)/2;
-        update(sti*2+1,stl,mid,i,amm);
-        update(sti*2+2,mid+1,str,i,amm);
-        st[sti] = f(st[sti*2+1],st[sti*2+2]);
+        update(left(sti),stl,mid,i,amm);
+        update(right(sti),mid+1,str,i,amm);
+        st[sti] = f(st[left(sti)],st[right(sti)]);
     }
     public:
-        SegTree(int n):  st(4*n,0), lazy(4*n,0){size = n;}
+        SegTree(vi& v) : st(4*v.size(),0), lazy(4*v.size(),0) {size = v.size(); build(0,0,size - 1, v);}
+        SegTree(int n) : st(4*n,0), lazy(4*n,0){size = n;}
         int query(int l, int  r){return query(0,0,size-1,l,r);}
         void update_range(int l, int r, int amm){update_range(0,0,size-1,l,r,amm);}
         void update(int i, int amm){update(0,0,size-1,i,amm);}
