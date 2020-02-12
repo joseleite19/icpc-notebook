415c5cec1fe44ad2e2cdc364b57d9ae380f3f193
José Marcos da Silva Leite <leite.josemarcos@gmail.com>
2020-02-12 04:04:57 -0300

    Update submasks.cpp

--- code/misc/submasks.cpp
+++ code/misc/submasks.cpp
@@ -1,6 +1,10 @@
 // loop through all submask of a given bitmask
 // it does not include mask 0
-for(int sub = mask; sub; sub = (sub-1)&mask){
+for(int sub = mask; sub; sub = (sub - 1) & mask){
 
 }
 
+// loop through all supermasks of a given bitmask
+for(int super = mask; super < (1 << n); super = (super + 1) | mask) {
+  
+}
