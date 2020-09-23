a341d911b2c27408af312fd8803d13332545147f
Jose Leite <leite.josemarcos@gmail.com>
2020-09-22 23:18:39 -0300

    trying landscape format

--- code/misc/rand.cpp
+++ code/misc/rand.cpp
@@ -2,10 +2,5 @@
 #include <chrono>
 cout << RAND_MAX << endl;
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-vector<int> permutation(N);
-iota(permutation.begin(), permutation.end(), 0);
-shuffle(permutation.begin(), permutation.end(), rng);
-iota(permutation.begin(), permutation.end(), 0);
-for(int i = 1; i < N; i++){
-    swap(permutation[i], permutation[uniform_int_distribution<int>(0, i)(rng)]);
-}
+shuffle(p.begin(), p.end(), rng);
+uniform_int_distribution<int>(a,b)(rng);
--- vimrc
+++ vimrc
@@ -1,5 +1,6 @@
 set ts=4 sw=4 sta nu rnu sc stl+=%F cindent
 set bg=dark ruler timeoutlen=1000
+set viminfo='20,\"1000
 imap {<CR> {<CR>}<Esc>O
 nmap <F2> 0V$%d
 nmap <C-down> :m+1<CR>
@@ -9,3 +10,5 @@ nmap <S-up> :m-2<CR>
 nmap <S-down> :m+1<CR>
 vmap <C-c> "+y
 syntax on
+
+set timeoutlen=1000
